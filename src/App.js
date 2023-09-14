d351cad9bc7214d353cbeac119befa32429e4f8b
Austin Amaral <austin.amaral3@gmail.com>
2023-09-14 10:46:57 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
+import EditMovieForm from './components/EditMovieForm';
+import AddMovieForm from './components/AddMovieForm';
 
 import FavoriteMovieList from './components/FavoriteMovieList';
 
@@ -25,6 +27,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id) => {
+      setMovies(movies.filter(movie => movie.id !== Number(id)));
   }
 
   const addToFavorites = (movie) => {
@@ -43,9 +46,11 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies} />
 
           <Routes>
-            <Route path="movies/edit/:id" />
+            <Route path="movies/edit/:id" element={<EditMovieForm setMovies={setMovies}/>}/>
 
-            <Route path="movies/:id" />
+            <Route path="/movies/add" element={<AddMovieForm setMovies={setMovies}/>}/>
+
+            <Route path="movies/:id" element={<Movie deleteMovie={deleteMovie}/>}/>
 
             <Route path="movies" element={<MovieList movies={movies} />} />
 
